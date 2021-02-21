84bcc04fca6e2550cf96bf4d8e6686755051dbbc
Qi Yang <nayamama@hotmail.com>
2021-02-20 22:29:48 -0500

    change the view cycle time and decrease the multi-threads time

--- multiple_process.sh
+++ multiple_process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1..15};
+for i in {1..5};
   do python3 view.py >> view_log.log &
 done
 wait
--- view.py
+++ view.py
@@ -8,10 +8,23 @@ current_time = time.strftime("%m/%d/%Y, %H:%M:%S")
 print(current_time)
 print("=======================================")
 
-media = ["https://v.qq.com/x/cover/mzc00200js3mdvw/j00357b0cuz.html",
+# The urls for all possible videos
+media = ["https://v.qq.com/x/cover/mzc00200js3mdvw/q00354i139r.html",
          "https://v.qq.com/x/cover/mzc00200js3mdvw/j00357b0cuz.html",
          "https://v.qq.com/x/cover/mzc00200js3mdvw/p003524sofe.html",
-         "https://v.qq.com/x/cover/mzc00200js3mdvw/a00353f725d.html"]
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/a00353f725d.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/s003537tpor.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/n003541yb8q.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/r0035tg5brg.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/l0035pno4wy.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/h00350k7goi.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/q0035cv7esm.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/u0035nkcd9y.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/l00357clii4.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/h0035ixn0dz.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/e0035a9lm9l.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/w00353nzvuh.html",
+         "https://v.qq.com/x/cover/mzc00200js3mdvw/t0035iwvazc.html"]
 
 # Start headless Google driver
 options = Options()
@@ -29,8 +42,10 @@ driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver",
 
 for i in range(10):
     print("Running the video for {} time".format(i))
-    driver.get("https://v.qq.com/x/page/w3228bfxa8z.html")
-    sleep_time = random.randint(10, 25)
+    #driver.get("https://v.qq.com/x/page/w3228bfxa8z.html")
+    video_id = random.randint(0, len(media) - 1)
+    driver.get(media[video_id])
+    sleep_time = random.randint(1200, 1320)
     time.sleep(sleep_time)
 
 driver.close()
